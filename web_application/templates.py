447797967b86de382a7ee93b783b0fe218c3f24c
yka85 <young-min_kim@sfu.ca>
2019-11-19 20:41:28 -0800

    inserted sample data

--- web_application/templates.py
+++ web_application/templates.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template, url_for
-app = Flask(__name__)
 
+app = Flask(__name__)
 web_name = 'Web Crawl Style Creator'
+
+# sample returned results 
+title_font = ['Gibson','Gibson','Gibson']
+body_font  = ['Times','Courier New','Helvetica Neue']
+back_color = ['#5ee682','#d4e8ff','#c389cc']
+text_color = ['#7a5835','#8a8563','#444444']
+link_color = ['#2c451d','#529ECC','#9767a1']
+
+popular_topics = ['popular topic 1', 'popular topic 2', 'popular topic 3', 'popular topic 4', 'popular topic 5']
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-	return render_template('home.html', name=web_name)
+	return render_template('home.html', name=web_name, popular=popular_topics)
 
 @app.route('/about')
 def about():
-	return render_template('about.html', title='About', name=web_name)
+	return render_template('about.html', title='About', name=web_name, popular=popular_topics)
 
 @app.route('/result')
 def result():
-	return render_template('result.html', name=web_name)
+	return render_template('result.html', name=web_name, \
+		title_font=title_font, body_font=body_font, \
+		back_color=back_color, text_color=text_color, link_color=link_color, \
+		popular=popular_topics)
 
 if __name__ == '__main__':
 	app.run(debug=True)
